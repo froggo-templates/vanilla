9629ea4ae2a7bb651c5b6daddad37ef0a9fe9378
eb <9453858-eb3n@users.noreply.gitlab.com>
2022-02-03 16:21:06 +0000

    feat: add IE11/IMT support

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,39 @@
 import json from '@rollup/plugin-json'
 import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+
+const ie11Plugins = [
+  babel({
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          useBuiltIns: 'usage',
+          corejs: 3,
+          targets: {
+            browsers: ['last 2 versions', 'ie >= 11']
+          }
+        }
+      ]
+    ]
+  }),
+  nodeResolve(),
+  commonjs()
+]
 
 module.exports = {
   input: 'src/main.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  },
+  output: { file: 'dist/bundle.js', format: 'cjs' },
   plugins: [
     json(),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            modules: false,
-            targets: {
-              browsers: ['last 2 versions', 'ie >= 11']
-            }
-          }
-        ]
-      ]
-    }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    // ...ie11Plugins /* uncomment to support IE11 */,
     terser()
   ]
 }
