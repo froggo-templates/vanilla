77391813be748b95dc8d0c4a383330e5078bbd91
eb <9453858-eb3n@users.noreply.gitlab.com>
2022-02-04 11:36:11 +0000

    feat: add _template-data directory

--- .eleventy.js
+++ .eleventy.js
@@ -4,17 +4,18 @@ module.exports = function (config) {
   // reload on file changes
   config.addWatchTarget('./src')
 
-  // copy assets into dist/assets/, except json files
+  // copy assets/ (except json files) into dist/assets/
   config.addPassthroughCopy('src/assets/**/*.!(json)')
 
-  // copy public assets into dist/
+  // copy public/ assets into dist/
   config.addPassthroughCopy({ 'src/public/!(*.gitkeep)': '.' })
 
   return {
     dir: {
       input: 'src',
       output: 'dist',
-      includes: '_partials'
+      includes: '_partials',
+      data: '_template-data'
     }
   }
 }
